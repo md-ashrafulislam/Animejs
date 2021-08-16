d56e21a677984ffdfa5a7aa0a1a1b3c44041e534
md-ashrafulislam <ksashraful94@gmail.com>
2021-08-16 12:51:14 +0600

    animation create use animejs

---
+++ animation1/js/app.js
@@ -0,0 +1,12 @@
+anime({
+    targets: '.box2',
+    translateX: 350,
+    rotateZ: 360,
+    scale: 3,
+    duration: 2000,
+    loop: true,
+    delay: 1000,
+    endDelay: 500,
+    direction: 'alternate',
+
+  });
---
+++ animation2/js/app.js
@@ -0,0 +1,17 @@
+
+anime({
+    targets: '.box2',
+    translateX: 350,
+    rotateZ: 360,
+    scale: 3,
+    duration: 3000,
+    loop: true,
+    easing: 'easeInOutExpo',
+    backgroundColor: '#FFF',
+    borderRadius: ['0%', '50%'],
+    delay: 500,
+    endDelay: 10,
+    direction: 'alternate',
+
+  });
+
